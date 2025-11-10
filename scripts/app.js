16ea5679a1e6b94756ea75db03c699f392d3cd03
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-11-10 10:20:14 -0800

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+//Comments in JS use two forward slashes
+
+// alert("JS IS WORKING")
+console.log("This is the message from the app.js");
+
+//This is how would declare a variable in c#
+// string myName = "Ken";
+
+//We know that c# is a strict a programming language
+
+//JS is a loosely typed programming language
+//We use the let prefix to declare variables when we would need to change the variable.
+
+let myString = "Ken";
+let anotherString = 'Martinez'; // JS will allow both single and double quotes for string;
+
+let myInt = 4;          //This is an interger;
+let myBool = true; // This is boolean
+
+let myNull = null; // This is a null value
+
+myString = "Karla";
+console.log(myString);
+
+const pi = 3.14; //Const variable cannot be 
+// pi = 4; 
+
+console.log(myInt + pi);
+console.log(myString + myInt);
+
+
+let myArray = [1,2,3,4, "Five", true];
+
+
+console.log(myArray);
+
+let myElement = document.getElementById("myElement")
+
+console.log(myElement);
+myElement.textContent = "I changed the text from JS! "
